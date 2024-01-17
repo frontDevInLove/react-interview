b8a340cf94474a66291698a2c8989ad38d5a20cb
frontDevInLove <153988119+frontDevInLove@users.noreply.github.com>
2024-01-17 14:36:47 +0300

    Инит задачи

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import Main from "../pages/Main/Main";
+
+interface Props {
+}
+
+const App: FC<Props> = () => {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import Task from "../../features/task/Task";
+
+interface Props {
+}
+
+const Main: FC<Props> = () => {
+  return (
+    <>
+      <Task/>
+    </>
+  );
+};
+
+export default Main;
